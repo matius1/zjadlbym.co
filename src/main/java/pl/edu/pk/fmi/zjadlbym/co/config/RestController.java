5859fbe97767a145ed23009212654528a17a4172
Mateusz <matius1@o2.pl>
2019-01-07 14:30:50 +0100

    Cookies part 2

--- src/main/java/pl/edu/pk/fmi/zjadlbym/co/config/RestController.java
+++ src/main/java/pl/edu/pk/fmi/zjadlbym/co/config/RestController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
@@ -14,7 +13,6 @@ import pl.edu.pk.fmi.zjadlbym.co.model.RecipesHolder;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.*;
@@ -24,20 +22,15 @@ import static org.springframework.http.HttpStatus.OK;
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/przepis")
 public class RestController {
-    String cookieName = "zjadlbym.co";
-
-    private Map<String, List<Map<Instant, String>>> sessions = new HashMap<>();
-
-    private static final String RECIPE_URL = "http://www.recipepuppy.com/api/?i=";
     private Logger logger = LoggerFactory.getLogger(RestController.class);
 
-    private RestTemplate restTemplate;
-    private ObjectMapper objectMapper;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private RestTemplate restTemplate = new RestTemplate();
 
-    public RestController() {
-        restTemplate = new RestTemplate();
-        objectMapper = new ObjectMapper();
-    }
+    private static final String RECIPE_URL = "http://www.recipepuppy.com/api/?i=";
+
+    private final String cookieName = "zjadlbym.co";
+    private Map<String, List<Map<Instant, String>>> sessions = new HashMap<>();
 
     @RequestMapping("/history")
     public List historyGet(HttpServletRequest request) {
@@ -52,12 +45,10 @@ public class RestController {
     }
 
     @RequestMapping("/get")
-    public ResponseEntity<RecipeDto[]> przepisyGet(String ingredients, HttpServletRequest request) {
+    public ResponseEntity przepisyGet(String ingredients, HttpServletRequest request) {
         logger.info("Request: {}", ingredients);
 
-        Cookie cookie = WebUtils.getCookie(request, cookieName);
-        String sessionID = cookie != null ? cookie.getValue() : "";
-        HttpHeaders sessionHeaders = manageSessions(ingredients, sessionID);
+        HttpHeaders sessionHeaders = manageSessions(ingredients, request);
 
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(RECIPE_URL + ingredients, String.class);
 
@@ -66,37 +57,42 @@ public class RestController {
                 RecipesHolder recipesHolder = objectMapper.readValue(responseEntity.getBody(), RecipesHolder.class);
                 RecipeDto[] dtos = GetRecipeDtos(recipesHolder);
 
-                return new ResponseEntity<>(dtos, HttpStatus.OK);
+                return ResponseEntity.ok().headers(sessionHeaders).body(dtos);
 
             } catch (IOException e) {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                return ResponseEntity.badRequest().headers(sessionHeaders).build();
             }
         }
 
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return ResponseEntity.noContent().headers(sessionHeaders).build();
     }
 
-    private HttpHeaders manageSessions(String ingredients, String cookie) {
-        if (cookie.isEmpty()) {
-            double id = new Random().nextDouble();
-            String newCookie = String.valueOf(id);
-            addToHistory(ingredients, cookie, newCookie);
+    private HttpHeaders manageSessions(String ingredients, HttpServletRequest request) {
+        Cookie cookie = WebUtils.getCookie(request, cookieName);
+        String sessionID = cookie != null ? cookie.getValue() : "";
+        HttpHeaders returnCookie = new HttpHeaders();
+
+        if (sessionID.isEmpty()) {
+            String newID = String.valueOf(UUID.randomUUID());
+            addToHistory(ingredients, newID);
+            returnCookie.add("Set-Cookie", cookieName + "=" + newID);
+
         } else {
-            addToHistory(ingredients, cookie, cookie);
+            addToHistory(ingredients, sessionID);
+            returnCookie.add("Set-Cookie", cookieName + "=" + sessionID);
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Set-Cookie", cookieName + "=" + "value");
-        return headers;
+        return returnCookie;
     }
 
-    private void addToHistory(String ingredients, String cookie, String newCookie) {
+    private void addToHistory(String ingredients, String sessionID) {
         Map<Instant, String> record = Collections.singletonMap(Instant.now(), ingredients);
-        List<Map<Instant, String>> history = sessions.getOrDefault(cookie, new ArrayList<>());
+        List<Map<Instant, String>> history = sessions.getOrDefault(sessionID, new ArrayList<>());
         history.add(record);
-        logger.info("Add to session cache : {} - {}", newCookie, record);
 
-        sessions.put(newCookie, history);
+        logger.info("Add to session cache : {} - {}", sessionID, record);
+
+        sessions.put(sessionID, history);
     }
 
     private RecipeDto[] GetRecipeDtos(RecipesHolder recipesHolder) {
